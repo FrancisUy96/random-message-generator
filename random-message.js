a5f42f36e300857049eb470a65dbe3186191ca03
FrancisUy96 <149303216+FrancisUy96@users.noreply.github.com>
2024-03-31 20:44:49 +1100

    Change Author

--- random-message.js
+++ random-message.js
@@ -22,7 +22,7 @@ const Messages = {
     ["The only way to do great work is to love what you do.", "Steve Jobs"],
     ["Success is not final, failure is not fatal: It is the courage to continue that counts.", "Winston Churchill"],
     ["The future belongs to those who believe in the beauty of their dreams.", "Eleanor Roosevelt"],
-    ["Believe you can and you're halfway there.", "Theodore Roosevelt"],
+    ["Believe you can and you're halfway there.", "Theodore"],
   ],
 };
 
